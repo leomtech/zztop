4f955d77b27aa03ccd7ddf38bbef25a882d73910
Leonardo Gonzalez <leonardogonzalez@mtech-systems.com>
2023-02-21 09:07:27 -0600

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: 'home.page.html'
 })
 export class HomePage {
-
   constructor() {}
 
+  entity = {
+    decimal: 0,
+    numeric: 0,
+    prBR: 0,
+    enUS: 0,
+    decimal_pr_BR: 0,
+    numeric_en_US: 0
+  };
+
+  onClick() {
+    console.log(JSON.stringify(this.entity))
+  }
 }
